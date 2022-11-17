c77c4056b6f7fe39b19ea80fb88beb823f443983
Wilson Wu <wuyecheng738@gmail.com>
2022-11-17 16:01:47 -0500

    some recap1

---
+++ recap_exercise_1/lib/problems.rb
@@ -0,0 +1,121 @@
+# Write a method, all_vowel_pairs, that takes in an array of words and returns all pairs of words
+# that contain every vowel. Vowels are the letters a, e, i, o, u. A pair should have its two words
+# in the same order as the original array. 
+#
+# Example:
+#
+# all_vowel_pairs(["goat", "action", "tear", "impromptu", "tired", "europe"])   # => ["action europe", "tear impromptu"]
+def all_vowel_pairs(words)
+    pairs = []
+    (0...words.length).each do |idx1|
+        hash_1 = vowel_count(words[idx1])
+        (idx1+1...words.length).each do |idx2|
+            hash_2 = vowel_count(words[idx2])
+            total = hash_1.merge(hash_2)
+            check = total.keys.sort.join
+            if check == "aeiou"
+                pairs.push([words[idx1], words[idx2]].join(" "))
+            end
+            hash_2 = {}
+        end
+        hash_1 = {}
+    end
+    return pairs
+end
+
+
+def vowel_count(word)
+    hash = Hash.new(0)
+    vowels = "aeiou"
+    word.each_char do |char|
+        hash[char] += 1 if vowels.include?(char)
+    end
+    hash
+end
+#p all_vowel_pairs(["goat", "action", "tear", "impromptu", "tired", "europe"])
+
+
+# Write a method, composite?, that takes in a number and returns a boolean indicating if the number
+# has factors besides 1 and itself
+#
+# Example:
+#
+# composite?(9)     # => true
+# composite?(13)    # => false
+def composite?(num)
+    (2...num).each do |n|
+        return true if num % n == 0
+    end
+    false
+end
+
+
+# A bigram is a string containing two letters.
+# Write a method, find_bigrams, that takes in a string and an array of bigrams.
+# The method should return an array containing all bigrams found in the string.
+# The found bigrams should be returned in the the order they appear in the original array.
+#
+# Examples:
+#
+# find_bigrams("the theater is empty", ["cy", "em", "ty", "ea", "oo"])  # => ["em", "ty", "ea"]
+# find_bigrams("to the moon and back", ["ck", "oo", "ha", "at"])        # => ["ck", "oo"]
+def find_bigrams(str, bigrams)
+    # new_arr = []
+    # (0...str.length).each do |idx1|
+    #     (idx1+1...str.length).each do |idx2|
+    #         new_arr << str[idx1..idx2] if bigrams.include?(str[idx1..idx2])
+    #     end
+    # end
+    # new_arr.sort
+    bigrams.select { |ele| str.include?(ele)}
+end
+
+class Hash
+    # Write a method, Hash#my_select, that takes in an optional proc argument
+    # The method should return a new hash containing the key-value pairs that return
+    # true when passed into the proc.
+    # If no proc is given, then return a new hash containing the pairs where the key is equal to the value.
+    #
+    # Examples:
+    #
+    # hash_1 = {x: 7, y: 1, z: 8}
+    # hash_1.my_select { |k, v| v.odd? }          # => {x: 7, y: 1}
+    #
+    # hash_2 = {4=>4, 10=>11, 12=>3, 5=>6, 7=>8}
+    # hash_2.my_select { |k, v| k + 1 == v }      # => {10=>11, 5=>6, 7=>8})
+    # hash_2.my_select                            # => {4=>4}
+    def my_select(&prc)
+        prc ||= Proc.new { |k, v| k == v}
+        self.select { |k, v| prc.call(k,v) == true}
+    end
+end
+
+class String
+    # Write a method, String#substrings, that takes in a optional length argument
+    # The method should return an array of the substrings that have the given length.
+    # If no length is given, return all substrings.
+    #
+    # Examples:
+    #
+    # "cats".substrings     # => ["c", "ca", "cat", "cats", "a", "at", "ats", "t", "ts", "s"]
+    # "cats".substrings(2)  # => ["ca", "at", "ts"]
+    def substrings(length = nil)
+
+
+
+    end
+
+
+    # Write a method, String#caesar_cipher, that takes in an a number.
+    # The method should return a new string where each char of the original string is shifted
+    # the given number of times in the alphabet.
+    #
+    # Examples:
+    #
+    # "apple".caesar_cipher(1)    #=> "bqqmf"
+    # "bootcamp".caesar_cipher(2) #=> "dqqvecor"
+    # "zebra".caesar_cipher(4)    #=> "difve"
+    def caesar_cipher(num)
+        alphabet = ("a".."z").to_a
+    end
+end
